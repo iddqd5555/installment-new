afba4d752cb00acaeb9a813c4b3a671ccb5e0035
iddqd5555 <origin334455@gmail.com>
2025-06-21 03:50:11 +0700

    fix: update Filament login blade for v3 component prefix

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,21 +1,21 @@
 {{-- resources/views/admin/login.blade.php --}}
-<x-filament::layouts.auth>
+<x-filament-panels::layouts.auth>
     <x-filament-panels::form wire:submit="authenticate" class="space-y-8">
-        <x-filament::input.wrapper>
-            <x-filament::input.label for="prefix" :value="'Prefix'" />
-            <x-filament::input.text wire:model.lazy="prefix" id="prefix" autofocus />
-            <x-filament::input.error for="prefix" />
-        </x-filament::input.wrapper>
-        <x-filament::input.wrapper>
-            <x-filament::input.label for="username" :value="'Username'" />
-            <x-filament::input.text wire:model.lazy="username" id="username" />
-            <x-filament::input.error for="username" />
-        </x-filament::input.wrapper>
-        <x-filament::input.wrapper>
-            <x-filament::input.label for="password" :value="'Password'" />
-            <x-filament::input.text wire:model.lazy="password" id="password" type="password" />
-            <x-filament::input.error for="password" />
-        </x-filament::input.wrapper>
-        <x-filament::button type="submit" class="w-full">เข้าสู่ระบบ</x-filament::button>
+        <x-filament-panels::input.wrapper>
+            <x-filament-panels::input.label for="prefix" :value="'Prefix'" />
+            <x-filament-panels::input.text wire:model.lazy="prefix" id="prefix" autofocus />
+            <x-filament-panels::input.error for="prefix" />
+        </x-filament-panels::input.wrapper>
+        <x-filament-panels::input.wrapper>
+            <x-filament-panels::input.label for="username" :value="'Username'" />
+            <x-filament-panels::input.text wire:model.lazy="username" id="username" />
+            <x-filament-panels::input.error for="username" />
+        </x-filament-panels::input.wrapper>
+        <x-filament-panels::input.wrapper>
+            <x-filament-panels::input.label for="password" :value="'Password'" />
+            <x-filament-panels::input.text wire:model.lazy="password" id="password" type="password" />
+            <x-filament-panels::input.error for="password" />
+        </x-filament-panels::input.wrapper>
+        <x-filament-panels::button type="submit" class="w-full">เข้าสู่ระบบ</x-filament-panels::button>
     </x-filament-panels::form>
-</x-filament::layouts.auth>
+</x-filament-panels::layouts.auth>
